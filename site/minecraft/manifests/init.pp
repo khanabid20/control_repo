d799a27346f485255920214c7f4c4b904340318e
Abid Khan <33711981+khanabid20@users.noreply.github.com>
2022-05-21 23:06:07 +0530

    Update init.pp
    Correct the curly braces

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraft {
   file {'/opt/minecraft':
     ensure => directory,
   }
-  file ('/opt/minecraft/minecraft_server.jar':
+  file {'/opt/minecraft/minecraft_server.jar':
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
   }
